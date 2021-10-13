f108cd3423e72c4eb5b52957a91cf4fb98369486
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2021-10-13 17:05:47 +0200

    fix new skill problem

--- src/components/skill/skill-edit.component.jsx
+++ src/components/skill/skill-edit.component.jsx
@@ -8,16 +8,17 @@ class SkillEditComponent extends Component {
 
         this.state = {
             gotoSkillList: props.gotoSkillList,
-            id: props.id,
+            id: parseInt(props.id),
             name: "",
             description: "Level 1:\nLevel 2:\nLevel 3:"
         };
     }
 
     componentDidMount = () => {
-        if (this.props.id) {
+        if (this.props.id && typeof(this.props.id) == 'number') {
+            console.log("SkillEditComponent.componentDidMount("+this.props.id+")");
             this.editMode = true;
-            let tmpSkill = SkillService.getSkill(parseInt(this.props.id));
+            let tmpSkill = SkillService.getSkill(parseInt(this.staprops.id));
             console.log("SkillEditComponent.componentDidMount tmpSkill:" + tmpSkill)
             this.setState({ id: tmpSkill.id, name: tmpSkill.name, description: tmpSkill.description });
         } else {
