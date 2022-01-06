e93266353a352595f1b9a7827b916f52f827dff2
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2022-01-06 20:46:34 +0100

    minor changes

--- src/components/skillassignment/skill-assignment-list-view.component.jsx
+++ src/components/skillassignment/skill-assignment-list-view.component.jsx
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import EmployeeService from "../../service/employee.service";
 import EnricherService from "../../service/enricher.service";
 import skillAssignmentService from "../../service/skill-assignment.service";
 import SkillService from "../../service/skill.service";
@@ -8,14 +7,15 @@ class SkillAssignmentListViewComponent extends Component {
     constructor() {
         super();
 
-        this.state = { assignments: [], enrichedAssignments: [] };
+        this.state = { assignments: [], enrichedAssignments: [], skills:[], skillGroups: [] };
     }
 
     componentDidMount() {
         let assignments = skillAssignmentService.getAssignments();
         let enrichedAssignments = EnricherService.enrichAssignments(assignments);
-
-        this.setState({ assignments, enrichedAssignments });
+        let skillGroups = SkillService.getSkillGroupList();
+        let skills = SkillService.getSkillList();
+        this.setState({ assignments, enrichedAssignments, skills, skillGroups });
     }
 
     deleteAssignment = (id) => {
@@ -24,20 +24,43 @@ class SkillAssignmentListViewComponent extends Component {
         const idx = enrichedAssignments.findIndex((assignment) => assignment.id === id);
         enrichedAssignments.splice(idx, 1);
 
-        this.setState({enrichedAssignments});
+        this.setState({ enrichedAssignments });
     }
 
     render() {
         return (<div>
             <h3>SKillAssignmentListViewComponent</h3>
             <table>
-                {this.state.enrichedAssignments.map((assignment) => {
-                    return <tr>
-                        <td>{assignment.name}:</td>
-                        <td>{assignment.skills.map((skill) => skill.skillName + '(' + skill.skillLevel + ') ')}</td>
-                        <td><button>edit</button> &nbsp; <button onClick={()=> this.deleteAssignment(assignment.id)}>delete</button></td>
-                    </tr>
-                })}
+                <thead>
+                    <th>Name filter:<br/><input type="text" id="nameFilter"></input></th>
+                    <th>
+                        SkillFilter:<br/>
+                        <select>
+                            <option>select filter</option>
+                            {this.state.skillGroups.map((group) => {
+                                return <option key={group.id}>{group.name}</option>
+                            })}
+                            <option>select all</option>
+                        </select>
+                        <select>
+                            <option>select filter</option>
+                            {this.state.skills.map((skill) => {
+                                return <option key={skill.id} >{skill.name}</option>
+                            })}
+                        </select>
+                    </th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    {this.state.enrichedAssignments.map((assignment) => {
+                        return <tr>
+                            <td>{assignment.name}:</td>
+                            <td>{assignment.skills.map((skill) => skill.skillName + '(' + skill.skillLevel + ') ')}</td>
+                            <td><button>edit</button> &nbsp; <button onClick={() => this.deleteAssignment(assignment.id)}>delete</button></td>
+                        </tr>
+
+                    })}
+                </tbody>
             </table>
         </div>
         )
