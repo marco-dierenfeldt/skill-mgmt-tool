f5eff04405360238ee7d5eaeb7f4331aaff70985
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2022-01-07 23:49:17 +0100

    layout employee edit component

--- src/components/employee/employee-edit.component.jsx
+++ src/components/employee/employee-edit.component.jsx
@@ -6,30 +6,31 @@ class EmployeeEditComponent extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { 
+        this.state = {
             gotoSkillList: props.gotoEmployeeList,
             id: props.id,
-            name: "", 
-            surname: "", 
-            unit: "", 
-            role: "" };
+            name: "",
+            surname: "",
+            unit: "",
+            role: ""
+        };
     }
 
     componentDidMount = () => {
-        if (this.props.id && typeof(this.props.id) == 'number') {
+        if (this.props.id && typeof (this.props.id) == 'number') {
             this.editMode = true;
             let tmpEmployee = EmployeeService.getEmployee(parseInt(this.props.id));
             this.setState({
-                id: tmpEmployee.id, 
-                name: tmpEmployee.name, 
-                surname: tmpEmployee.surname, 
-                unit: tmpEmployee.unit, 
+                id: tmpEmployee.id,
+                name: tmpEmployee.name,
+                surname: tmpEmployee.surname,
+                unit: tmpEmployee.unit,
                 role: tmpEmployee.role,
                 title: 'Edit existing employee'
             });
         } else {
             this.editMode = false;
-            this.setState({title: 'Add new employee'});
+            this.setState({ title: 'Add new employee' });
         }
     }
 
@@ -50,7 +51,7 @@ class EmployeeEditComponent extends Component {
     }
 
     handleSubmit = (event) => {
-        if (this.editMode) { 
+        if (this.editMode) {
             EmployeeService.updateSEmployee(this.state.id, this.state.name, this.state.name, this.state.unit, this.state.role);
         } else {
             EmployeeService.addEmployee(this.state.name, this.state.surname, this.state.unit, this.state.role);
@@ -62,70 +63,66 @@ class EmployeeEditComponent extends Component {
     btnTxt = () => {
         if (this.editMode) {
             return 'Update';
-         } else {
-             return 'Submit';
-         }
+        } else {
+            return 'Submit';
+        }
     }
 
     render = () => {
         return (
             <form onSubmit={this.handleSubmit}>
-                <h3>{this.state.title}</h3>
-                <table>
-                    <tr>
-                        <td>Vorname</td>
-                        <td>
-                            <input
-                                name="name"
-                                type="text"
-                                value={this.state.name}
-                                onChange={e => this.setName(e.target.value)}
-                                required />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nachname</td>
-                        <td>
-                            <input
-                                name="surname"
-                                type="text"
-                                value={this.state.surname}
-                                onChange={e => this.setSurname(e.target.value)}
-                                required />
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>Einheit</td>
-                        <td>
-                            <input
-                                name="unit"
-                                type="text"
-                                value={this.state.unit}
-                                onChange={e => this.setUnit(e.target.value)}
-                                required>
-                            </input>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>Rolle</td>
-                        <td>
-                            <input
-                                name="role"
-                                type="text"
-                                value={this.state.role}
-                                onChange={e => this.setRole(e.target.value)}
-                                required>
-                            </input>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td></td><td align="right"><button>{this.btnTxt()}</button></td>
-                    </tr>
-                </table>
-            </form>
+                <h3 className="title is-3" >{this.state.title}</h3>
+                <div className="field">
+                    <label className="label">Name</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            name="name"
+                            type="text"
+                            value={this.state.name}
+                            onChange={e => this.setName(e.target.value)}
+                            required />
+                    </div>
+                </div><div className="field">
+                    <label className="label">Surname</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            name="surname"
+                            type="text"
+                            value={this.state.surname}
+                            onChange={e => this.setSurname(e.target.value)}
+                            required />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Unit</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            name="unit"
+                            type="text"
+                            value={this.state.unit}
+                            onChange={e => this.setUnit(e.target.value)}
+                            required>
+                        </input>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Role</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            name="role"
+                            type="text"
+                            value={this.state.role}
+                            onChange={e => this.setRole(e.target.value)}
+                            required>
+                        </input>
+                    </div>
+                </div>
+                <button className="button is-primary" >{this.btnTxt()}</button>
+            </form >
         );
 
     }
