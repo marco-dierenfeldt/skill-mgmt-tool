b3024bd86fb09f34ff81494b12858ef3bb0fbc27
marco-dierenfeldt <marco.dierenfeldt@gmail.com>
2022-01-07 23:11:38 +0100

    finish skill edit component

--- src/components/employee/employee-overview.component.jsx
+++ src/components/employee/employee-overview.component.jsx
@@ -6,8 +6,8 @@ class EmployeeOverviewComponent extends Component {
     render = () => {
         return (
             <div>
-                <h3>Employee Overview</h3>
-                <button onClick={this.props.gotoEmployeeEdit}>Create new Employee</button>
+                <h3 className="title is-3">Employee Overview</h3>
+                <button className="button is-primary" onClick={this.props.gotoEmployeeEdit}>Create new Employee</button>
                 <EmployeeListViewComponent editEmployee={this.props.gotoEmployeeEdit}/>
             </div>);
     }
